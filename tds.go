46f36551e6a20c3d0767161475284c069dadb081
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2019-10-22 12:15:10 +0100

    allow proxying login

--- tds.go
+++ tds.go
@@ -912,3 +912,122 @@ loginEnd:
 	}
 	return &sess, nil
 }
+
+// writes the prelogin response as though from the mssql backend
+func writePreloginResponse(w *tdsBuffer, fields map[uint8][]byte) error {
+	var err error
+
+	w.BeginPacket(4, false)
+	offset := uint16(5*len(fields) + 1)
+	keys := make(keySlice, 0, len(fields))
+	for k, _ := range fields {
+		keys = append(keys, k)
+	}
+	sort.Sort(keys)
+	// writing header
+	for _, k := range keys {
+		err = w.WriteByte(k)
+		if err != nil {
+			return err
+		}
+		err = binary.Write(w, binary.BigEndian, offset)
+		if err != nil {
+			return err
+		}
+		v := fields[k]
+		size := uint16(len(v))
+		err = binary.Write(w, binary.BigEndian, size)
+		if err != nil {
+			return err
+		}
+		offset += size
+	}
+	err = w.WriteByte(preloginTERMINATOR)
+	if err != nil {
+		return err
+	}
+	// writing values
+	for _, k := range keys {
+		v := fields[k]
+		written, err := w.Write(v)
+		if err != nil {
+			return err
+		}
+		if written != len(v) {
+			return errors.New("Write method didn't write the whole value")
+		}
+	}
+	return w.FinishPacket()
+}
+
+// reads the client prelogin
+func readClientPrelogin(r *tdsBuffer) (map[uint8][]byte, error) {
+	_, err := r.BeginRead()
+	if err != nil {
+		return nil, err
+	}
+
+	struct_buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	offset := 0
+	results := map[uint8][]byte{}
+	for true {
+		rec_type := struct_buf[offset]
+		if rec_type == preloginTERMINATOR {
+			break
+		}
+
+		rec_offset := binary.BigEndian.Uint16(struct_buf[offset+1:])
+		rec_len := binary.BigEndian.Uint16(struct_buf[offset+3:])
+		value := struct_buf[rec_offset : rec_offset+rec_len]
+		results[rec_type] = value
+		offset += 5
+	}
+	return results, nil
+}
+
+// PerformFakeHandshakeForClient carries out a fake handshake with a client
+// 1. reads the prelogin packet
+// 2. writes the prelogin repsonse declaring NO support for encryption
+// 3. reads the login packet
+// 4. writes the login response as a success. TODO: what about failure ?
+func PerformFakeHandshakeForClient(clientConn net.Conn) error {
+	// using the default packet size of 4096 (see go-mssqldb/conn_str.go)
+	clientBuf := newTdsBuffer(4096, clientConn)
+	msg, err := readClientPrelogin(clientBuf)
+	if err != nil {
+		return fmt.Errorf("bad prelogin: %s", err)
+	}
+
+	// based on wireshark, the prelogin response from the server is almost identical
+	// to the login message from the client, with minor changes as below:
+	msg[preloginTHREADID] = []byte{}
+	msg[preloginVERSION] = []byte{0x0e, 0x00, 0x0c, 0xa6, 0x00, 0x00}
+	msg[preloginENCRYPTION] = []byte{encryptNotSup}
+	err = writePreloginResponse(clientBuf, msg)
+	if err != nil {
+		return fmt.Errorf("bad prelogin response: %s", err)
+	}
+
+	// we actually don't care what the client has to say.
+	// we just need for them to not be blocked
+	err = clientBuf.readNextPacket()
+	if err != nil {
+		return fmt.Errorf("bad client login read: %s", err)
+	}
+
+	// created an unencrypted connection to mssql, and just copied the raw bytes
+	// this has loginack, and done tokens.
+	_, err = clientConn.Write([]byte{
+		0x04,0x01,0x00,0x64,0x00,0x37,0x01,0x00,0xad,0x36,0x00,0x01,0x74,0x00,0x00,0x04,0x16,0x4d,0x00,0x69,0x00,0x63,0x00,0x72,0x00,0x6f,0x00,0x73,0x00,0x6f,0x00,0x66,0x00,0x74,0x00,0x20,0x00,0x53,0x00,0x51,0x00,0x4c,0x00,0x20,0x00,0x53,0x00,0x65,0x00,0x72,0x00,0x76,0x00,0x65,0x00,0x72,0x00,0x00,0x00,0x00,0x00,0x0e,0x00,0x0c,0xa6,0xe3,0x13,0x00,0x04,0x04,0x34,0x00,0x30,0x00,0x39,0x00,0x36,0x00,0x04,0x34,0x00,0x30,0x00,0x39,0x00,0x36,0x00,0xfd,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	})
+
+	if err != nil {
+		return fmt.Errorf("bad client write login response: %s", err)
+	}
+
+	return nil
+}
